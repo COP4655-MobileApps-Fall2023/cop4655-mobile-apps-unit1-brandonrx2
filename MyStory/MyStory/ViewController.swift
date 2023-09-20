35737cb60ac2789005c0c02a4ea5b5149f3a1ff9
brandonrx2 <124232197+brandonrx2@users.noreply.github.com>
2023-09-19 22:29:16 -0400

    Add files via upload
    Fix constraint issues and change font and display colors

---
+++ MyStory/MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Brandon Rojas on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
